2e64718e5ff0b5584fc02ccf38b26d0e54499a27
Deras879 <derycard59@gmail.com>
2023-11-23 19:51:30 -0500

    Finalizados modelos de BDD

---
+++ server/src/models/Activity.js
@@ -0,0 +1,35 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "Activity",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      difficulty: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          min: 1,
+          max: 5,
+          isInt: true,
+        },
+      },
+      duration: {
+        type: DataTypes.TIME,
+      },
+      season: {
+        type: DataTypes.ENUM("Verano", "Otoño", "Invierno", "Primavera"),
+        allowNull: false,
+      },
+    },
+    { timestamps: false }
+  );
+};
--- server/src/models/Country.js
+++ server/src/models/Country.js
@@ -1,12 +1,55 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('Country', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "Country",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      id: {
+        primaryKey: true,
+        type: DataTypes.STRING(3),
+        allowNull: false,
+        unique: true,
+        validate: {
+          len: [3, 3],
+          is: /^[a-zA-Z]{3}$/,
+        },
+      },
+      continent: {
+        type: DataTypes.STRING(60),
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          is: /\.(jpg|jpeg|png|gif)$/i,
+        },
+      },
+      capital: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      subRegion: {
+        type: DataTypes.STRING,
+      },
+      area: {
+        type: DataTypes.DECIMAL(10, 2),
+        validate: {
+          isDecimal: true,
+          min: 0,
+        },
+      },
+      population: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
     },
-  });
+    { timestamps: false }
+  );
 };
