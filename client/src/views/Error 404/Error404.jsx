151f95bbc3e08b2d0ef02d3cf7cd7a4d22a9a890
Deras879 <derycard59@gmail.com>
2023-12-12 23:37:50 -0500

    Mejorado vista error 404, eliminados css predeterminados de vite

--- client/src/views/Error 404/Error404.jsx
+++ client/src/views/Error 404/Error404.jsx
@@ -1,14 +1,9 @@
 import React from "react";
 import style from "./Error404.module.css";
+const image = "Imagen de WhatsApp 2023-12-12 a las 23.09.24_31f83bf3.jpg";
 
 const Error404 = () => {
-  return (
-    <img
-      className={style.img}
-      src="https://th.bing.com/th/id/OIG.2qiSL7DDr4SZBMPNZnP1?w=1024&h=1024&rs=1&pid=ImgDetMain"
-      alt=""
-    />
-  );
+  return <img className={style.img} src={image} alt="" />;
 };
 
 export default Error404;
